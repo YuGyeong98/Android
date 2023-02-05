b1b3f0fc3a722620346313a1617ee8c4bbf584b9
YuGyeong98 <kuk3097@naver.com>
2023-02-05 17:59:53 +0900

    feat: 단위 변환기 앱 구현

--- app/src/main/java/com/example/unit_converter/MainActivity.kt
+++ app/src/main/java/com/example/unit_converter/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.unit_converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import com.example.unit_converter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var inputNumber = 0
+    var cmToM = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val inputEditText = binding.inputEditText
+        val outputTextView = binding.outputTextView
+        val inputUnitTextView = binding.inputUnitTextView
+        val outputUnitTextView = binding.outputUnitTextView
+        val swapImageButton = binding.swapImageButton
+
+        inputEditText.addTextChangedListener { text ->
+            inputNumber = if (text.isNullOrEmpty()) 0 else text.toString().toInt()
+
+            if (cmToM) {
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+
+        swapImageButton.setOnClickListener {
+            cmToM = !cmToM
+            if (cmToM) {
+                inputUnitTextView.text = "cm"
+                outputUnitTextView.text = "m"
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                inputUnitTextView.text = "m"
+                outputUnitTextView.text = "cm"
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
     }
 }

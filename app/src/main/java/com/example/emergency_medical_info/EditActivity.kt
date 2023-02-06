03af33d7d8f41e719ac4fbe7598917490efb2287
YuGyeong98 <kuk3097@naver.com>
2023-02-06 22:24:03 +0900

    feat: edit 화면 생년월일, 혈액형, 체크박스 구현

--- app/src/main/java/com/example/emergency_medical_info/EditActivity.kt
+++ app/src/main/java/com/example/emergency_medical_info/EditActivity.kt
@@ -1,11 +1,37 @@
 package com.example.emergency_medical_info
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.core.view.isVisible
+import com.example.emergency_medical_info.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.birthdayLayer.setOnClickListener {
+            val listener = OnDateSetListener { _, year, month, dayOfMonth ->
+                binding.birthdayValueTextView.text = "$year-${month.inc()}-$dayOfMonth"
+            }
+            DatePickerDialog(this, listener, 2023, 1, 6).show()
+        }
+
+        binding.bloodTypeSpinner.adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.blood_types,
+            android.R.layout.simple_list_item_1
+        )
+
+        binding.precautionsCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            binding.precautionsEditText.isVisible = isChecked
+        }
+        binding.precautionsEditText.isVisible = binding.precautionsCheckBox.isChecked
     }
 }

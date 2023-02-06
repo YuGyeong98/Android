a02d1b3c9bee4d920bd7153db1de5205b4663cf1
YuGyeong98 <kuk3097@naver.com>
2023-02-06 22:41:05 +0900

    feat: edit 화면 사용자 정보 저장 구현

--- app/src/main/java/com/example/emergency_medical_info/EditActivity.kt
+++ app/src/main/java/com/example/emergency_medical_info/EditActivity.kt
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog.OnDateSetListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.core.view.isVisible
 import com.example.emergency_medical_info.databinding.ActivityEditBinding
 
@@ -33,5 +34,30 @@ class EditActivity : AppCompatActivity() {
             binding.precautionsEditText.isVisible = isChecked
         }
         binding.precautionsEditText.isVisible = binding.precautionsCheckBox.isChecked
+
+        binding.saveButton.setOnClickListener {
+            saveData()
+            finish()
+        }
     }
+
+    private fun saveData() {
+        with(getSharedPreferences(USER_INFORMATION, MODE_PRIVATE).edit()) {
+            putString(NAME, binding.nameEditText.text.toString())
+            putString(BIRTHDAY, binding.birthdayValueTextView.text.toString())
+            putString(BLOOD_TYPE, getBloodType())
+            putString(EMERGENCY_CONTACT, binding.emergencyContactEditText.text.toString())
+            putString(PRECAUTIONS, getPrecautions())
+        }
+        Toast.makeText(this, "저장 완료", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getBloodType(): String {
+        val bloodAlphabet = binding.bloodTypeSpinner.selectedItem.toString()
+        val bloodSign = if (binding.bloodTypePlus.isChecked) "+" else "-"
+        return "$bloodSign$bloodAlphabet"
+    }
+
+    private fun getPrecautions() =
+        if (binding.precautionsCheckBox.isChecked) binding.precautionsEditText.text.toString() else ""
 }

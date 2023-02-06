a079b1ea222dcde07b47b7557d8b6cfedb90b1ec
YuGyeong98 <kuk3097@naver.com>
2023-02-06 21:52:40 +0900

    feat: main에서 edit으로 이동

--- app/src/main/java/com/example/emergency_medical_info/MainActivity.kt
+++ app/src/main/java/com/example/emergency_medical_info/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.emergency_medical_info
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.emergency_medical_info.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.editButton.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

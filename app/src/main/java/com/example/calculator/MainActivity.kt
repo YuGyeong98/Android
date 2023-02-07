91bee266b2422ea8a8a9d1d365dfa1eded5bb9c8
YuGyeong98 <kuk3097@naver.com>
2023-02-07 23:40:36 +0900

    feat: equal 버튼 구현

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.example.calculator.databinding.ActivityMainBinding
+import java.math.BigDecimal
 import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
@@ -56,6 +57,39 @@ class MainActivity : AppCompatActivity() {
 
         updateEquationTextView()
         binding.resultTextView.text = ""
+    }
+
+    fun equalClicked(view: View) {
+        if (firstNumberText.isEmpty() || secondNumberText.isEmpty() || operatorText.isEmpty()) {
+            Toast.makeText(this, "완성되지 않은 수식입니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val firstNumber = firstNumberText.toString().toBigDecimal()
+        val secondNumber = secondNumberText.toString().toBigDecimal()
+
+        binding.resultTextView.text = when (operatorText.toString()) {
+            "+" -> decimalFormat.format(firstNumber + secondNumber)
+            "-" -> decimalFormat.format(firstNumber - secondNumber)
+            "×" -> decimalFormat.format(firstNumber * secondNumber)
+            "÷" -> decimalFormat.format(
+                if (secondNumber != BigDecimal("0")) {
+                    firstNumber / secondNumber
+                } else {
+                    Toast.makeText(this, "0으로 나눌 수 없어요.", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            )
+            "%" -> decimalFormat.format(
+                if (secondNumber != BigDecimal("0")) {
+                    firstNumber % secondNumber
+                } else {
+                    Toast.makeText(this, "0으로 나눌 수 없어요.", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            )
+            else -> "잘못된 수식 입니다."
+        }
     }
 
     private fun updateEquationTextView() {

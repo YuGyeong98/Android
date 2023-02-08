6aa89c763f0689b205a75ed31268d21c7a05a1eb
YuGyeong98 <kuk3097@naver.com>
2023-02-08 21:39:14 +0900

    feat: 카운트다운 dialog 초기 생성

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.example.stopwatch.databinding.ActivityMainBinding
+import com.example.stopwatch.databinding.DialogCountdownSettingBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.countdownTextView.setOnClickListener {
+            showCountdownSettingDialog()
+        }
+    }
+
+    private fun showCountdownSettingDialog() {
+        AlertDialog.Builder(this).apply {
+            val dialogBinding = DialogCountdownSettingBinding.inflate(layoutInflater)
+            with(dialogBinding.countdownSecondPicker) {
+                maxValue = 20
+                minValue = 0
+            }
+            setTitle("카운트다운 설정")
+            setView(dialogBinding.root)
+            setPositiveButton("확인", null)
+            setNegativeButton("취소", null)
+        }.show()
     }
 }

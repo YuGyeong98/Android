b577cc7af2828810c098ea504b9a400be6765fd3
YuGyeong98 <kuk3097@naver.com>
2023-02-15 15:07:00 +0900

    feat: RecyclerView main 화면에 연결

--- app/src/main/java/com/example/vocabulary/MainActivity.kt
+++ app/src/main/java/com/example/vocabulary/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.vocabulary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.vocabulary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var wordAdapter: WordAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        wordAdapter = WordAdapter(mutableListOf(Word("apple", "사과", "명사"), Word("banana", "바나나", "명사")))
+        binding.wordRecyclerView.apply {
+            adapter = wordAdapter
+            layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            val divider = DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
+            addItemDecoration(divider)
+        }
     }
 }

e5b1b6b5f7dfe3070c41360cebba057cd253e260
YuGyeong98 <kuk3097@naver.com>
2023-02-26 18:28:48 +0900

    feat: 앨범 생성 화면에 ViewPager2 UI 적용

--- app/src/main/java/com/example/image_album/FrameActivity.kt
+++ app/src/main/java/com/example/image_album/FrameActivity.kt
@@ -1,11 +1,22 @@
 package com.example.image_album
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.image_album.databinding.ActivityFrameBinding
 
 class FrameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFrameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_frame)
+        binding = ActivityFrameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val images = (intent.getStringArrayExtra("images") ?: emptyArray()).map {
+            FrameItem(Uri.parse(it))
+        }
+        val frameAdapter = FrameAdapter(images)
+        binding.viewPager.adapter = frameAdapter
     }
 }

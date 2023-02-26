d153d8e81d693a8b164dc5db775c7fd3f7cfbbde
YuGyeong98 <kuk3097@naver.com>
2023-02-26 14:30:52 +0900

    feat: 이미지 읽기 권한 추가

--- app/src/main/java/com/example/image_album/MainActivity.kt
+++ app/src/main/java/com/example/image_album/MainActivity.kt
@@ -1,11 +1,82 @@
 package com.example.image_album
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.image_album.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val imageLoadLauncher =
+        registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList ->
+            updateImage(uriList)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loadImageButton.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_MEDIA_IMAGES
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                loadImage()
+            }
+            shouldShowRequestPermissionRationale(
+                Manifest.permission.READ_MEDIA_IMAGES
+            ) -> {
+                showPermissionInfoDialog()
+            }
+            else -> {
+                requestReadMediaImages()
+            }
+        }
+    }
+
+    private fun loadImage() {
+        imageLoadLauncher.launch("image/\\*")
+    }
+
+    private fun showPermissionInfoDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("이미지를 가져오기 위해 미디어 이미지 읽기 권한이 필요합니다.")
+            setPositiveButton("확인") { _, _ ->
+                requestReadMediaImages()
+            }
+            setNegativeButton("취소", null)
+        }.show()
+    }
+
+    private fun requestReadMediaImages() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_MEDIA_IMAGES),
+                REQUEST_READ_MEDIA_IMAGES
+            )
+        }
+    }
+
+    private fun updateImage(uriList: List<Uri>) {
+        Log.i("updateImage", "$uriList")
+    }
+
+    companion object {
+        const val REQUEST_READ_MEDIA_IMAGES = 100
     }
 }

babfa1924ca2ecd17e0dbe563b3032084fdacfe8
YuGyeong98 <kuk3097@naver.com>
2023-02-26 18:42:01 +0900

    feat: ViewPager2와 tabLayout 연결

--- app/src/main/java/com/example/image_album/FrameActivity.kt
+++ app/src/main/java/com/example/image_album/FrameActivity.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.image_album.databinding.ActivityFrameBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class FrameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFrameBinding
@@ -18,5 +19,9 @@ class FrameActivity : AppCompatActivity() {
         }
         val frameAdapter = FrameAdapter(images)
         binding.viewPager.adapter = frameAdapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            binding.viewPager.currentItem = tab.position
+        }.attach()
     }
 }

c57674b1dbeca056fbd736189410d4529971e652
YuGyeong98 <kuk3097@naver.com>
2023-03-01 11:16:09 +0900

    feat: 인텐트 액션 전달 후 음악 플레이어 서비스 시작

--- app/src/main/java/com/example/music_player/MainActivity.kt
+++ app/src/main/java/com/example/music_player/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.music_player
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.music_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pauseButton.setOnClickListener { pause() }
+        binding.playButton.setOnClickListener { play() }
+        binding.stopButton.setOnClickListener { stop() }
+    }
+
+    private fun pause() {
+        Intent(this, MediaPlayerService::class.java).also { intent ->
+            intent.action = PAUSE
+            startService(intent)
+        }
+    }
+
+    private fun play() {
+        Intent(this, MediaPlayerService::class.java).also { intent ->
+            intent.action = PLAY
+            startService(intent)
+        }
+    }
+
+    private fun stop() {
+        Intent(this, MediaPlayerService::class.java).also { intent ->
+            intent.action = STOP
+            startService(intent)
+        }
     }
 }

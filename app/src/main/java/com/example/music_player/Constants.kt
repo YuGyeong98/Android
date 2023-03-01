9e3f53efcff09adb5607e7ad6a3ebd2bf153c283
YuGyeong98 <kuk3097@naver.com>
2023-03-01 11:15:00 +0900

    feat: Service 생성 후, 음악 재생 및 일시 정지 구현

---
+++ app/src/main/java/com/example/music_player/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.music_player
+
+const val PAUSE = "pause"
+const val PLAY = "play"
+const val STOP = "stop"
---
+++ app/src/main/java/com/example/music_player/MediaPlayerService.kt
@@ -0,0 +1,31 @@
+package com.example.music_player
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+
+class MediaPlayerService : Service() {
+    private var mediaPlayer: MediaPlayer? = null
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when(intent?.action) {
+            PAUSE -> {
+                mediaPlayer?.pause()
+            }
+            PLAY -> {
+                if (mediaPlayer == null) {
+                    mediaPlayer = MediaPlayer.create(this, R.raw.icefire)
+                }
+                mediaPlayer?.start()
+            }
+            STOP -> {
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+}

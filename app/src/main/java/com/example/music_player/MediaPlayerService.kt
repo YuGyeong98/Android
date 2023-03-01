031f965339e0f78e7a221283a899e0cca315da14
YuGyeong98 <kuk3097@naver.com>
2023-03-01 12:04:51 +0900

    feat: 화면이 꺼진 상태에서도 음악 재생 유지

--- app/src/main/java/com/example/music_player/MediaPlayerService.kt
+++ app/src/main/java/com/example/music_player/MediaPlayerService.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.IBinder
+import android.os.PowerManager
 
 class MediaPlayerService : Service() {
     private var mediaPlayer: MediaPlayer? = null
@@ -19,9 +20,12 @@ class MediaPlayerService : Service() {
             }
             PLAY -> {
                 if (mediaPlayer == null) {
-                    mediaPlayer = MediaPlayer.create(this, R.raw.icefire)
+                    mediaPlayer = MediaPlayer.create(applicationContext, R.raw.icefire)
                 }
-                mediaPlayer?.start()
+                mediaPlayer?.apply {
+                    start()
+                    setWakeMode(applicationContext, PowerManager.PARTIAL_WAKE_LOCK)
+                }
             }
             STOP -> {
                 mediaPlayer?.stop()

dcb4bfc680d64b56147286ec0a066156401adae9
YuGyeong98 <kuk3097@naver.com>
2023-03-02 12:23:38 +0900

    feat: Notification 권한 추가 및 채널 생성

--- app/src/main/java/com/example/music_player/MediaPlayerService.kt
+++ app/src/main/java/com/example/music_player/MediaPlayerService.kt
@@ -1,6 +1,9 @@
 package com.example.music_player
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.IBinder
@@ -9,12 +12,23 @@ import android.os.PowerManager
 class MediaPlayerService : Service() {
     private var mediaPlayer: MediaPlayer? = null
 
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        when(intent?.action) {
+        when (intent?.action) {
             PAUSE -> {
                 mediaPlayer?.pause()
             }

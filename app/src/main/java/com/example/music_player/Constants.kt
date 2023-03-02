a0393186791cb0e78abdcb3ad32c7ed3ec4a4b55
YuGyeong98 <kuk3097@naver.com>
2023-03-02 12:24:09 +0900

    fix: Constants 추가

--- app/src/main/java/com/example/music_player/Constants.kt
+++ app/src/main/java/com/example/music_player/Constants.kt
@@ -1,5 +1,7 @@
 package com.example.music_player
 
+const val CHANNEL_ID = "MEDIA_PLAYER_CHANNEL"
+const val CHANNEL_NAME = "MEDIA_PLAYER"
 const val PAUSE = "pause"
 const val PLAY = "play"
 const val STOP = "stop"

2f001063cdf4f59d231a66cc18fbabd396e08221
YuGyeong98 <kuk3097@naver.com>
2023-03-03 11:54:07 +0900

    feat: Notification 권한 체크

--- app/src/main/java/com/example/music_player/MainActivity.kt
+++ app/src/main/java/com/example/music_player/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.music_player
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.music_player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,9 +19,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.pauseButton.setOnClickListener { pause() }
-        binding.playButton.setOnClickListener { play() }
-        binding.stopButton.setOnClickListener { stop() }
+        checkPermission()
     }
 
     private fun pause() {
@@ -36,6 +40,48 @@ class MainActivity : AppCompatActivity() {
         Intent(this, MediaPlayerService::class.java).also { intent ->
             intent.action = STOP
             startService(intent)
+        }
+    }
+
+    private fun checkPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                binding.pauseButton.setOnClickListener { pause() }
+                binding.playButton.setOnClickListener { play() }
+                binding.stopButton.setOnClickListener { stop() }
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) -> {
+                showPermissionInfoDialog()
+            }
+            else -> {
+                requestPostNotification()
+            }
+        }
+    }
+
+    private fun showPermissionInfoDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("알림을 표시하기 위해 노티피케이션 권한이 필요합니다.")
+            setPositiveButton("확인") { _, _ ->
+                requestPostNotification()
+            }
+            setNegativeButton("취소", null)
+        }.show()
+    }
+
+    private fun requestPostNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                NOTIFICATION_ID
+            )
         }
     }
 }

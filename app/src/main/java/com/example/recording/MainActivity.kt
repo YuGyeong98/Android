33e869bd2b212fbf33413ebbe529e43563cb0786
YuGyeong98 <kuk3097@naver.com>
2023-03-16 22:28:03 +0900

    feat: 녹음 버튼을 눌렀을 때, 권한 체크

--- app/src/main/java/com/example/recording/MainActivity.kt
+++ app/src/main/java/com/example/recording/MainActivity.kt
@@ -1,11 +1,109 @@
 package com.example.recording
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.recording.databinding.ActivityMainBinding
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recordButton.setOnClickListener {
+            checkPermission()
+        }
+    }
+
+    private fun checkPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // 녹음 시작
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) -> {
+                showPermissionRationaleDialog()
+            }
+            else -> {
+                requestRecordAudio()
+            }
+        }
+    }
+
+    private fun showPermissionRationaleDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("녹음 권한을 허용해야 앱을 정상적으로 사용할 수 있습니다.")
+            setPositiveButton("권한 허용하기") { _, _ ->
+                requestRecordAudio()
+            }
+            setNegativeButton("취소", null)
+        }.show()
+    }
+
+    private fun requestRecordAudio() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.RECORD_AUDIO),
+            REQUEST_RECORD_AUDIO_PERMISSION
+        )
+    }
+
+    private fun showPermissionSettingDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("녹음 권한을 허용해야 앱을 정상적으로 사용할 수 있습니다. 앱 설정 화면에서 권한을 켜주세요.")
+            setPositiveButton("권한 변경하기") { _, _ ->
+                navigateToAppSetting()
+            }
+            setNegativeButton("취소", null)
+        }.show()
+    }
+
+    private fun navigateToAppSetting() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+        }
+        startActivity(intent)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_RECORD_AUDIO_PERMISSION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // 녹음 시작
+                } else if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        Manifest.permission.RECORD_AUDIO
+                    )
+                ) {
+                    showPermissionRationaleDialog()
+                } else {
+                    showPermissionSettingDialog()
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_RECORD_AUDIO_PERMISSION = 200
     }
 }

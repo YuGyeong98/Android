75e5cb1c076296af408a2b3405c3c522e47cb9b9
YuGyeong98 <kuk3097@naver.com>
2023-03-25 22:11:32 +0900

    feat: 클라이언트 생성

--- app/src/main/java/com/example/client_server/MainActivity.kt
+++ app/src/main/java/com/example/client_server/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.example.client_server
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
+import com.example.client_server.databinding.ActivityMainBinding
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var serverHost = ""
+        val client = OkHttpClient()
+
+        binding.serverHostEditText.addTextChangedListener {
+            serverHost = it.toString()
+        }
+
+        binding.connectButton.setOnClickListener {
+            val request = Request.Builder()
+                .url("http://$serverHost:8080")
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "수신 실패", Toast.LENGTH_SHORT).show()
+                    }
+                    Log.e("CLIENT", e.toString())
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        val res = response.body?.string()
+                        runOnUiThread {
+                            binding.connectTextView.apply {
+                                isVisible = true
+                                text = res
+                            }
+                            binding.serverHostEditText.isVisible = false
+                            binding.connectButton.isVisible = false
+                        }
+                    } else {
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, "수신 실패", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            })
+        }
     }
 }

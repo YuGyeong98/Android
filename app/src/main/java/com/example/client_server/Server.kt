fde0fb9a49cfbac7d1ba746378e3984a66fa29a6
YuGyeong98 <kuk3097@naver.com>
2023-03-25 21:49:52 +0900

    feat: jvm에서 실행되는 서버 생성

---
+++ app/src/main/java/com/example/client_server/Server.kt
@@ -0,0 +1,37 @@
+package com.example.client_server
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+
+// jvm 위에서 서버 실행하기
+// (localhost:8080 || 127.0.0.1:8080 || ip주소:8080)을 입력하면 확인 가능
+fun main() {
+    Thread {
+        val port = 8080
+        val server = ServerSocket(port)
+
+        while (true) {
+            val socket = server.accept()
+            val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+            val printer = PrintWriter(socket.getOutputStream())
+
+            var input: String? = "-1"
+            while (input != null && input != "") {
+                input = reader.readLine()
+            }
+            println("READ DATA $input")
+
+            printer.println("HTTP/1.1 200 OK")
+            printer.println("Content-Type: text/html\r\n")
+            printer.println("<h1>Hello World</h1>")
+            printer.println("\r\n")
+            printer.flush()
+
+            printer.close()
+            reader.close()
+            socket.close()
+        }
+    }.start()
+}

d18c08bb8656cdc9e60b314535b7b59bcbd3e834
YuGyeong98 <kuk3097@naver.com>
2023-03-30 21:58:06 +0900

    feat: repository 화면 생성

---
+++ app/src/main/java/com/example/github_repository/RepoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.github_repository
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RepoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repo)
+    }
+}

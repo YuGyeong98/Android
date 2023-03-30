9ab133df6028f9e3bbd363aff0933a0235e28fad
YuGyeong98 <kuk3097@naver.com>
2023-03-30 22:23:31 +0900

    feat: REST API를 통해 가져온 데이터를 Repo RecyclerView에 표시

--- app/src/main/java/com/example/github_repository/RepoActivity.kt
+++ app/src/main/java/com/example/github_repository/RepoActivity.kt
@@ -1,11 +1,48 @@
 package com.example.github_repository
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.github_repository.adapter.RepoAdapter
+import com.example.github_repository.databinding.ActivityRepoBinding
+import com.example.github_repository.model.Repo
+import com.example.github_repository.network.GithubService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RepoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRepoBinding
+    private lateinit var repoAdapter: RepoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_repo)
+        binding = ActivityRepoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        repoAdapter = RepoAdapter()
+        binding.repoRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = repoAdapter
+        }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(GithubService::class.java)
+        service.listRepos("YuGyeong98").enqueue(object : Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                repoAdapter.submitList(response.body())
+            }
+
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+                Toast.makeText(this@RepoActivity, "에러가 발생하였습니다.", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }

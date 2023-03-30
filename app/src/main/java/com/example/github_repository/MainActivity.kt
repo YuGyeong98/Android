507085ac8a3a4bec690aba5d8f535064bd61ea13
YuGyeong98 <kuk3097@naver.com>
2023-03-30 21:21:47 +0900

    feat: REST API를 통해 가져온 데이터를 User RecyclerView에 표시

--- app/src/main/java/com/example/github_repository/MainActivity.kt
+++ app/src/main/java/com/example/github_repository/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.github_repository
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.github_repository.adapter.UserAdapter
+import com.example.github_repository.databinding.ActivityMainBinding
+import com.example.github_repository.model.UserDto
+import com.example.github_repository.network.GithubService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var userAdapter: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        userAdapter = UserAdapter()
+        binding.userRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = userAdapter
+        }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(GithubService::class.java)
+        service.searchUsers("YuGyeong98").enqueue(object : Callback<UserDto> {
+            override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
+                userAdapter.submitList(response.body()?.items)
+            }
+
+            override fun onFailure(call: Call<UserDto>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "에러가 발생하였습니다.", Toast.LENGTH_SHORT).show()
+                t.printStackTrace()
+            }
+        })
     }
 }

cf9e36ef09f812409d0bc1c672bfced7f5077fee
YuGyeong98 <kuk3097@naver.com>
2023-03-30 22:07:44 +0900

    feat: 'List repositories for a user' REST API 추가

--- app/src/main/java/com/example/github_repository/network/GithubService.kt
+++ app/src/main/java/com/example/github_repository/network/GithubService.kt
@@ -1,13 +1,19 @@
 package com.example.github_repository.network
 
+import com.example.github_repository.model.Repo
 import com.example.github_repository.model.UserDto
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubService {
     @Headers("Authorization: Bearer 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")
     @GET("search/users")
     fun searchUsers(@Query("q") query: String): Call<UserDto>
+
+    @Headers("Authorization: Bearer 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")
+    @GET("users/{username}/repos")
+    fun listRepos(@Path("username") username: String): Call<Repo>
 }

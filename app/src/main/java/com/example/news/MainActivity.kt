e10c836828dd1951655a26d8c81e79b669c71672
YuGyeong98 <kuk3097@naver.com>
2023-04-01 22:19:28 +0900

    feat: RecyclerView에 기사 제목 바인딩

--- app/src/main/java/com/example/news/MainActivity.kt
+++ app/src/main/java/com/example/news/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.news
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.news.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var newsAdapter: NewsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        newsAdapter = NewsAdapter()
+        binding.newsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = newsAdapter
+        }
+
+        val service = APIClient.retrofit.create(NewsService::class.java)
+        service.politicsNews().submitList()
+    }
+
+    private fun Call<News>.submitList() {
+        enqueue(object : Callback<News> {
+            override fun onResponse(call: Call<News>, response: Response<News>) {
+                newsAdapter.submitList(response.body()?.channel?.items)
+            }
+
+            override fun onFailure(call: Call<News>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }

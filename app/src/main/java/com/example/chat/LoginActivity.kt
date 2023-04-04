cebe0364ff1f06cc65751a838441bee827b769c1
YuGyeong98 <kuk3097@naver.com>
2023-04-04 22:51:07 +0900

    feat: 회원가입 및 로그인 구현

--- app/src/main/java/com/example/chat/LoginActivity.kt
+++ app/src/main/java/com/example/chat/LoginActivity.kt
@@ -1,11 +1,53 @@
 package com.example.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.chat.databinding.ActivityLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            Firebase.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                        Log.e("SIGN_UP", task.exception.toString())
+                    }
+                }
+        }
+        binding.signInButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            Firebase.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        Log.e("SIGN_IN", task.exception.toString())
+                    }
+                }
+        }
     }
 }

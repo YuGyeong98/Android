186b38d4af27771b5aa9704b85eb4edab32bd79a
YuGyeong98 <kuk3097@naver.com>
2023-04-06 13:02:54 +0900

    feat: 로그아웃 구현

--- app/src/main/java/com/example/chat/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/chat/mypage/MyPageFragment.kt
@@ -1,10 +1,14 @@
 package com.example.chat.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.chat.LoginActivity
 import com.example.chat.R
 import com.example.chat.databinding.FragmentMyPageBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MyPageFragment : Fragment(R.layout.fragment_my_page) {
     private lateinit var binding: FragmentMyPageBinding
@@ -12,5 +16,13 @@ class MyPageFragment : Fragment(R.layout.fragment_my_page) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMyPageBinding.bind(view)
+
+        binding.signOutButton.setOnClickListener {
+            Firebase.auth.signOut()
+
+            val intent = Intent(context, LoginActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
     }
 }

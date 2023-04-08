c2603fa572c2ae8f48ccbf6acc735a0b09441e4c
YuGyeong98 <kuk3097@naver.com>
2023-04-08 22:47:02 +0900

    feat: 채팅방에서 채팅 보냈을 때 데이터베이스 chats 생성 및 chatrooms 업데이트

--- chat/app/src/main/java/com/example/chat/chatroom/ChatRoomActivity.kt
+++ chat/app/src/main/java/com/example/chat/chatroom/ChatRoomActivity.kt
@@ -2,7 +2,15 @@ package com.example.chat.chatroom
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.chat.DBKey.Companion.DB_CHATS
+import com.example.chat.DBKey.Companion.DB_CHAT_ROOMS
+import com.example.chat.DBKey.Companion.DB_USERS
 import com.example.chat.databinding.ActivityChatRoomBinding
+import com.example.chat.userlist.UserItem
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ChatRoomActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChatRoomBinding
@@ -11,6 +19,42 @@ class ChatRoomActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityChatRoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val chatRoomId = intent.getStringExtra(CHAT_ROOM_ID) ?: return
+        val otherUserId = intent.getStringExtra(OTHER_USER_ID) ?: return
+        val currentUserId = Firebase.auth.currentUser?.uid ?: ""
+        var currentUsername = ""
+
+        Firebase.database.reference.child(DB_USERS).child(currentUserId).get()
+            .addOnSuccessListener {
+                val currentUser = it.getValue(UserItem::class.java)
+                currentUsername = currentUser?.username ?: ""
+            }
+
+        binding.sendButton.setOnClickListener {
+            val message = binding.messageEditText.text.toString()
+            if (message.isEmpty()) {
+                binding.sendButton.isEnabled = false
+            }
+
+            val newChatItem = ChatItem(
+                userId = currentUserId,
+                message = message,
+            )
+            Firebase.database.reference.child(DB_CHATS).child(chatRoomId).push().apply {
+                newChatItem.chatId = key
+                setValue(newChatItem)
+            }
+
+            val chatUpdates = hashMapOf<String, Any>(
+                "${DB_CHAT_ROOMS}/$currentUserId/$otherUserId/lastMessage" to message,
+                "${DB_CHAT_ROOMS}/$otherUserId/$currentUserId/lastMessage" to message,
+                "${DB_CHAT_ROOMS}/$otherUserId/$currentUserId/chatRoomId" to chatRoomId,
+                "${DB_CHAT_ROOMS}/$otherUserId/$currentUserId/otherUserId" to currentUserId,
+                "${DB_CHAT_ROOMS}/$otherUserId/$currentUserId/otherUsername" to currentUsername,
+            )
+            Firebase.database.reference.updateChildren(chatUpdates)
+        }
     }
 
     companion object {

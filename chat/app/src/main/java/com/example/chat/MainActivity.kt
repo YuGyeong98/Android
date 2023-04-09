ef0d3762cf2626d52ab9da4f71908552c2e9e3cd
YuGyeong98 <kuk3097@naver.com>
2023-04-09 22:22:39 +0900

    feat: fcm 알림 권한 요청

--- chat/app/src/main/java/com/example/chat/MainActivity.kt
+++ chat/app/src/main/java/com/example/chat/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.chat
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.chat.chatroomlist.ChatRoomListFragment
 import com.example.chat.databinding.ActivityMainBinding
@@ -28,6 +33,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        checkPostNotificationsPermission()
 
         binding.bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
@@ -57,5 +64,50 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.frameLayout, fragment)
             commit()
         }
+    }
+
+    private fun checkPostNotificationsPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED -> {
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.POST_NOTIFICATIONS
+            ) -> {
+                showPermissionRationaleDialog()
+            }
+            else -> {
+                requestPostNotifications()
+            }
+        }
+    }
+
+    private fun showPermissionRationaleDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("알림 권한을 허용해야 채팅 알림을 표시할 수 있습니다.")
+            setPositiveButton("권한 허용하기") { _, _ ->
+                requestPostNotifications()
+            }
+            setNegativeButton("취소") { dialogInterface, _ ->
+                dialogInterface.cancel()
+            }
+        }.show()
+    }
+
+    private fun requestPostNotifications() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                REQUEST_POST_NOTIFICATIONS_PERMISSION
+            )
+        }
+    }
+
+    companion object {
+        const val REQUEST_POST_NOTIFICATIONS_PERMISSION = 100
     }
 }

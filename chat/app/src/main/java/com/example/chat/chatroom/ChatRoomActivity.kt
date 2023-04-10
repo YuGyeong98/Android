0947ac2477811695eddbeeeee63709bb6f695513
YuGyeong98 <kuk3097@naver.com>
2023-04-10 20:55:46 +0900

    feat: fcm token 이용하여 메시지 받는 유저에게 알림 전송 후 response 확인

--- chat/app/src/main/java/com/example/chat/chatroom/ChatRoomActivity.kt
+++ chat/app/src/main/java/com/example/chat/chatroom/ChatRoomActivity.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chat.DBKey.Companion.DB_CHATS
 import com.example.chat.DBKey.Companion.DB_CHAT_ROOMS
 import com.example.chat.DBKey.Companion.DB_USERS
+import com.example.chat.R
 import com.example.chat.databinding.ActivityChatRoomBinding
 import com.example.chat.userlist.UserItem
 import com.google.firebase.auth.ktx.auth
@@ -16,11 +17,17 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import java.io.IOException
 
 class ChatRoomActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChatRoomBinding
     private lateinit var chatRoomAdapter: ChatRoomAdapter
     private var otherUserId = ""
+    private var otherFcmToken = ""
     private var chatRoomId = ""
     private val chatList = mutableListOf<ChatItem>()
 
@@ -66,6 +73,32 @@ class ChatRoomActivity : AppCompatActivity() {
             )
             Firebase.database.reference.updateChildren(chatUpdates)
 
+            val client = OkHttpClient()
+            val notification = JSONObject().apply {
+                put("title", getString(R.string.app_name))
+                put("body", message)
+            }
+            val root = JSONObject().apply {
+                put("to", otherFcmToken)
+                put("notification", notification)
+            }
+            val body = root.toString()
+                .toRequestBody("application/json; charset=utf-8".toMediaType())
+            val request = Request.Builder()
+                .header("Authorization", "key=${getString(R.string.fcm_server_key)}")
+                .url("https://fcm.googleapis.com/fcm/send")
+                .post(body)
+                .build()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    Log.e("CHAT", response.toString())
+                }
+            })
+
             binding.messageEditText.text.clear()
         }
 
@@ -79,6 +112,7 @@ class ChatRoomActivity : AppCompatActivity() {
     private fun getOtherUserData() {
         Firebase.database.reference.child(DB_USERS).child(otherUserId).get().addOnSuccessListener {
             val otherUser = it.getValue(UserItem::class.java)
+            otherFcmToken = otherUser?.fcmToken.orEmpty()
             chatRoomAdapter.otherUser = otherUser
 
             getChatData()

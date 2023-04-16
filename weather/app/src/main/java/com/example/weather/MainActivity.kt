2653136cdecaf422d5bcdf118fecb896071d15c6
YuGyeong98 <kuk3097@naver.com>
2023-04-16 22:52:02 +0900

    feat: main 화면에 현재 기온, 하늘상태, 강수확률 표시

--- weather/app/src/main/java/com/example/weather/MainActivity.kt
+++ weather/app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        WeatherRepository.getVillageForecast(
+            serviceKey = getString(R.string.weather_service_key),
+            latitude = 37.3592,
+            longitude = 127.1048,
+            successCallback = { forecastList ->
+                val currentForecast = forecastList.first()
+                binding.temperatureTextView.text = getString(R.string.temperature_text, currentForecast.tmp)
+                binding.skyTextView.text = currentForecast.sky
+                binding.precipitationTextView.text = getString(R.string.precipitation_text, currentForecast.pop)
+            },
+            failureCallback = { throwable ->
+                throwable.printStackTrace()
+            }
+        )
     }
 }

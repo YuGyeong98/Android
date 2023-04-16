93dead8d423016ff7d504a8e16c26fa9beec7591
YuGyeong98 <kuk3097@naver.com>
2023-04-16 19:50:55 +0900

    feat: 날씨 서비스 인터페이스 생성

---
+++ weather/app/src/main/java/com/example/weather/WeatherService.kt
@@ -0,0 +1,16 @@
+package com.example.weather
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+    @GET("1360000/VilageFcstInfoService_2.0/getVilageFcst?numOfRows=400&pageNo=1")
+    fun getVillageForecast(
+        @Query("serviceKey") serviceKey: String,
+        @Query("base_date") baseDate: String,
+        @Query("base_time") baseTime: String,
+        @Query("nx") nx: Int,
+        @Query("ny") ny: Int,
+    ): Call<WeatherEntity>
+}

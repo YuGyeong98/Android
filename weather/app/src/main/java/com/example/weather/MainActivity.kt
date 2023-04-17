028db1d40ef2662758856056fe0e1bfc7b351981
YuGyeong98 <kuk3097@naver.com>
2023-04-17 21:20:38 +0900

    feat: 위치 권한 요청

--- weather/app/src/main/java/com/example/weather/MainActivity.kt
+++ weather/app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.weather
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.weather.databinding.ActivityMainBinding
 import com.example.weather.databinding.ItemForecastBinding
 
@@ -13,6 +17,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        checkAccessCoarseLocationPermission()
+    }
+
+    private fun getVillageForecast() {
         WeatherRepository.getVillageForecast(
             serviceKey = getString(R.string.weather_service_key),
             latitude = 37.3592,
@@ -39,5 +47,67 @@ class MainActivity : AppCompatActivity() {
                 throwable.printStackTrace()
             }
         )
+    }
+
+    private fun checkAccessCoarseLocationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                getVillageForecast()
+            }
+            shouldShowRequestPermissionRationale(android.Manifest.permission.ACCESS_COARSE_LOCATION) -> {
+                showPermissionRationaleDialog()
+            }
+            else -> {
+                requestAccessCoarseLocation()
+            }
+        }
+    }
+
+    private fun showPermissionRationaleDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("위치 권한을 허용해야 날씨를 표시할 수 있습니다.")
+            setPositiveButton("확인") { _, _ ->
+                requestAccessCoarseLocation()
+            }
+            setNegativeButton("취소") { dialogInterface, _ ->
+                dialogInterface.cancel()
+            }
+        }.show()
+    }
+
+    private fun requestAccessCoarseLocation() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+            REQUEST_ACCESS_COARSE_LOCATION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_ACCESS_COARSE_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getVillageForecast()
+                } else {
+                    AlertDialog.Builder(this).apply {
+                        setMessage("위치 권한 거부로 인해 날씨를 표시할 수 없습니다. 설정 화면에서 직접 권한 허용을 해주세요.")
+                        setPositiveButton("확인", null)
+                    }.show()
+                }
+                return
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_ACCESS_COARSE_LOCATION = 100
     }
 }

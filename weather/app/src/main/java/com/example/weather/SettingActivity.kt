03d9b87191a2edb924611dcbe27ab28ff70486de
YuGyeong98 <kuk3097@naver.com>
2023-04-20 20:36:31 +0900

    feat: Setting 액티비티에서 권한 설정 버튼 클릭시 앱 설정 화면으로 이동

--- weather/app/src/main/java/com/example/weather/SettingActivity.kt
+++ weather/app/src/main/java/com/example/weather/SettingActivity.kt
@@ -1,11 +1,25 @@
 package com.example.weather
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import com.example.weather.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySettingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.settingButton.setOnClickListener {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.fromParts("package", packageName, null)
+            }
+            startActivity(intent)
+        }
     }
 }

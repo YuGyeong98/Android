68090f5b548818effbe16242a0d16566460e5010
YuGyeong98 <kuk3097@naver.com>
2023-04-20 22:25:01 +0900

    feat: 서비스를 포그라운드 서비스로 만들기

--- weather/app/src/main/java/com/example/weather/UpdateWeatherService.kt
+++ weather/app/src/main/java/com/example/weather/UpdateWeatherService.kt
@@ -1,15 +1,16 @@
 package com.example.weather
 
 import android.Manifest
-import android.app.PendingIntent
-import android.app.Service
+import android.app.*
 import android.appwidget.AppWidgetManager
 import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.IBinder
 import android.widget.RemoteViews
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
 import com.google.android.gms.location.LocationServices
 
 class UpdateWeatherService : Service() {
@@ -20,6 +21,9 @@ class UpdateWeatherService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         appWidgetManager = AppWidgetManager.getInstance(this)
+
+        createNotificationChannel()
+        startForeground(1, createNotification())
 
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -54,6 +58,11 @@ class UpdateWeatherService : Service() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        stopForeground(STOP_FOREGROUND_REMOVE)
+    }
+
     private fun updateAppWidget(
         requestCode: Int,
         temperature: CharSequence,
@@ -76,6 +85,28 @@ class UpdateWeatherService : Service() {
         val provider = ComponentName(this, WeatherAppWidgetProvider::class.java)
 
         appWidgetManager.updateAppWidget(provider, views)
+    }
+
+    private fun createNotification(): Notification {
+        return NotificationCompat.Builder(this, getString(R.string.channel_id))
+            .setSmallIcon(R.drawable.baseline_wb_sunny_24)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(getString(R.string.channel_text))
+            .build()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            getString(R.string.channel_id),
+            getString(R.string.app_name),
+            NotificationManager.IMPORTANCE_LOW
+        ).apply {
+            description = getString(R.string.channel_description)
+        }
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
     }
 
     companion object {

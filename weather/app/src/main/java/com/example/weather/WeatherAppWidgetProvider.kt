0024baadd313dad8ce480bfd82563b138d8a6656
YuGyeong98 <kuk3097@naver.com>
2023-04-20 22:23:46 +0900

    fix: 안드로이드 빌드 버전에 따른 예외 처리

--- weather/app/src/main/java/com/example/weather/WeatherAppWidgetProvider.kt
+++ weather/app/src/main/java/com/example/weather/WeatherAppWidgetProvider.kt
@@ -1,10 +1,12 @@
 package com.example.weather
 
+import android.app.ForegroundServiceStartNotAllowedException
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
 import androidx.core.content.ContextCompat
 import com.example.weather.UpdateWeatherService.Companion.REQUEST_WEATHER_INFO
@@ -18,7 +20,7 @@ class WeatherAppWidgetProvider : AppWidgetProvider() {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
         appWidgetIds.forEach { appWidgetId ->
             val pendingIntent = Intent(context, UpdateWeatherService::class.java).let {
-                PendingIntent.getService(
+                PendingIntent.getForegroundService(
                     context,
                     REQUEST_WEATHER_INFO,
                     it,
@@ -32,6 +34,14 @@ class WeatherAppWidgetProvider : AppWidgetProvider() {
         }
 
         val intent = Intent(context, UpdateWeatherService::class.java)
-        ContextCompat.startForegroundService(context, intent)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            try {
+                ContextCompat.startForegroundService(context, intent)
+            } catch (e: ForegroundServiceStartNotAllowedException) {
+                e.printStackTrace()
+            }
+        } else {
+            ContextCompat.startForegroundService(context, intent)
+        }
     }
 }

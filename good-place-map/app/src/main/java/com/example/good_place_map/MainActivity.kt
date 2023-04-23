869d911198e18632e1e5f3c4b6f964487a4d0f0b
YuGyeong98 <kuk3097@naver.com>
2023-04-23 21:59:22 +0900

    feat: 검색 결과 리사이클러뷰에 나타내기

--- good-place-map/app/src/main/java/com/example/good_place_map/MainActivity.kt
+++ good-place-map/app/src/main/java/com/example/good_place_map/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.good_place_map
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.good_place_map.databinding.ActivityMainBinding
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
     private lateinit var naverMap: NaverMap
+    private val searchResultAdapter = SearchResultAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,33 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                     fm.beginTransaction().add(R.id.map_fragment, it).commit()
                 }
         mapFragment.getMapAsync(this)
+
+        binding.bottomSheetLayout.searchResultRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = searchResultAdapter
+        }
+
+        binding.searchView.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query?.isNotEmpty() == true) {
+                    SearchRepository.getGoodPlace(query).enqueue(object : Callback<SearchResult> {
+                        override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
+                            searchResultAdapter.submitList(response.body()?.items)
+                        }
+
+                        override fun onFailure(call: Call<SearchResult>, t: Throwable) {
+                            t.printStackTrace()
+                        }
+                    })
+                    return false
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
     }
 
     override fun onMapReady(map: NaverMap) {

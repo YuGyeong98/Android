26b7dcf1628a0e0a7b96ed9acc6be0757a8e37b4
YuGyeong98 <kuk3097@naver.com>
2023-04-23 20:21:31 +0900

    feat: NaverMap 객체 얻어오기

--- good-place-map/app/src/main/java/com/example/good_place_map/MainActivity.kt
+++ good-place-map/app/src/main/java/com/example/good_place_map/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.good_place_map
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.good_place_map.databinding.ActivityMainBinding
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var naverMap: NaverMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment? ?: MapFragment.newInstance()
+                .also {
+                    fm.beginTransaction().add(R.id.map_fragment, it).commit()
+                }
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
     }
 }

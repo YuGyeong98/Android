2ac110d1a302c2db973a88e0154324a3c7d33e5b
YuGyeong98 <kuk3097@naver.com>
2023-04-25 23:08:18 +0900

    feat: retrofit response를 bundle로 전달하기 위해 parcelize 추가

--- good-place-map/app/src/main/java/com/example/good_place_map/SearchResult.kt
+++ good-place-map/app/src/main/java/com/example/good_place_map/SearchResult.kt
@@ -1,13 +1,17 @@
 package com.example.good_place_map
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @JsonClass(generateAdapter = true)
 data class SearchResult(
     @Json(name = "items") val items: List<SearchItem>
-)
+) : Parcelable
 
+@Parcelize
 @JsonClass(generateAdapter = true)
 data class SearchItem(
     @Json(name = "title") val title: String,
@@ -16,4 +20,4 @@ data class SearchItem(
     @Json(name = "roadAddress") val roadAddress: String,
     @Json(name = "mapx") val mapx: Int,
     @Json(name = "mapy") val mapy: Int,
-)
+) : Parcelable

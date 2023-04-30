384c370abc4e80cc66b4bc1c574bd8e7e059bedc
YuGyeong98 <kuk3097@naver.com>
2023-04-30 22:23:22 +0900

    refactor: 앱 난독화 방지를 위해 field 추가

--- good-place-map/app/src/main/java/com/example/good_place_map/SearchResult.kt
+++ good-place-map/app/src/main/java/com/example/good_place_map/SearchResult.kt
@@ -8,16 +8,16 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 @JsonClass(generateAdapter = true)
 data class SearchResult(
-    @Json(name = "items") val items: List<SearchItem>
+    @field:Json(name = "items") val items: List<SearchItem>
 ) : Parcelable
 
 @Parcelize
 @JsonClass(generateAdapter = true)
 data class SearchItem(
-    @Json(name = "title") val title: String,
-    @Json(name = "link") val link: String,
-    @Json(name = "category") val category: String,
-    @Json(name = "roadAddress") val roadAddress: String,
-    @Json(name = "mapx") val mapx: Int,
-    @Json(name = "mapy") val mapy: Int,
+    @field:Json(name = "title") val title: String,
+    @field:Json(name = "link") val link: String,
+    @field:Json(name = "category") val category: String,
+    @field:Json(name = "roadAddress") val roadAddress: String,
+    @field:Json(name = "mapx") val mapx: Int,
+    @field:Json(name = "mapy") val mapy: Int,
 ) : Parcelable
